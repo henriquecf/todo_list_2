941931b7bc59ce7d80c4033a9d630c3b3a0c63fe
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-10-21 10:58:01 -0200

    Included list model tests

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe List, :type => :model do
+  describe "should validate" do
+    it "presence of name" do
+      expect {List.create!}.to raise_error(/Name can't be blank/)
+    end
+
+    it "presence of user" do
+      expect {List.create!}.to raise_error(/User can't be blank/)
+    end
+  end
+
+  describe "nested tasks" do
+    before :each do
+      @user = User.create!(email: 'test@example.com', password: 'test1234')
+      @task = Task.new(task_name: 'task')
+      @list = List.create!(name: 'list', user: @user, tasks: [@task])
+    end
+    it "should allow creation of tasks within a list" do
+      expect(@list.reload.tasks.size).to eq(1)
+    end
+
+    it "should destroy tasks when their lists are destroyed" do
+      expect(Task.exists?(@task)).to be_truthy
+      @list.destroy
+      expect(Task.exists?(@task)).to be_falsey
+    end
+  end
+end
