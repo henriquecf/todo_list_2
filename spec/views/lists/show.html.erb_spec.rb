043aca4dd05cdb7cc1c6900feb10985119a6b292
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-10-21 11:17:40 -0200

    Did tests for show view

--- spec/views/lists/show.html.erb_spec.rb
+++ spec/views/lists/show.html.erb_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe "lists/show", :type => :view do
   before(:each) do
     @list = assign(:list, List.create!(name: "Test", user: User.create!(email: 'test@example.com', password: 'test1234')))
+    @list.tasks.create!(task_name: 'task')
   end
 
-  it "renders attributes in <p>" do
+  it "renders list name" do
     render
+    expect(rendered).to include('Test')
+  end
+  it "renders tasks" do
+    render
+    expect(rendered).to include('task')
   end
 end
