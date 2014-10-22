f0f6c945f4894ea066144de03160b236df1c169b
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2014-10-22 18:47:20 -0200

    Included list rejection for task with blank name

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,5 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :tasks, dependent: :destroy
-  accepts_nested_attributes_for :tasks, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :tasks, :reject_if => lambda { |task| task[:task_name].blank? }, :allow_destroy => true
 end
